a0ce76bf839ea04d57ecebd26a245e769c18557c
Leonie Nellesen <leonie.nellesen@gmail.com>
2025-11-10 16:49:20 +0100

    commit to checkout other branch

--- src/datenbank.py
+++ src/datenbank.py
@@ -1,22 +1,30 @@
 import sqlite3
+import pandas as pd
 
-connection = sqlite3.connect("../data/fahrerinnnen_test.db")
+# read in excel
+excel_path = "../data/Anmeldung_Landesmeisterschaft_2025.xlsx"   # update with your actual path
+df = pd.read_excel(excel_path, sheet_name=1, skiprows=4)
+
+
+
+print("Columns:", df.columns)
+
+connection = sqlite3.connect("../data/fahrerinnnen_test1.db")
 cursor = connection.cursor()
 
+# # uncomment below to create database
+# sql_command = """
+# CREATE TABLE fahrerinnen (
+# person_nummer INTEGER PRIMARY KEY,
+# name VARCHAR(50),
+# gender CHAR(1),
+# geburtsdatum DATE);"""
 
-#sql_command = """
-#CREATE TABLE fahrerinnen (
-#person_nummer INTEGER PRIMARY KEY,
-#name VARCHAR(50),
-#gender CHAR(1),
-#geburtsdatum DATE,
-#verein VARCHAR(50));"""
-
-#cursor.execute(sql_command)
+# cursor.execute(sql_command)
 
 
-#sql_command = """INSERT INTO fahrerinnen (person_nummer, name, gender, geburtsdatum, verein)
-#    VALUES (NULL, "Lara Müller", "w", "2002-07-23", "BW96 Schenefeld");"""
+#sql_command = """INSERT INTO fahrerinnen (person_nummer, name, gender, geburtsdatum)
+#    VALUES (NULL, "Lara Müller", "w", "2002-07-23");"""
 #cursor.execute(sql_command)
 
 # never forget this, if you want the changes to be saved:
