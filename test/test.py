bb4148d75906c753cbb3d9ae47293afdaccca58c
isabelbroeder <isabel.broeder@gmx.de>
2026-02-13 15:55:53 +0100

    new test set_age_group()

--- test/test.py
+++ test/test.py
@@ -1,6 +1,9 @@
 import datetime
 
+#from src.unicycle import functions, create_database
+#from src.unicycle import set_age_group
 from src.unicycle.functions import calculate_age
+from src.unicycle.create_database import set_age_group
 
 
 
@@ -14,6 +17,14 @@ def test_calculate_age_month():
 
 def test_calculate_age_day():
     assert 21 == calculate_age(datetime.datetime(2004,6, 27), datetime.datetime(2025,6, 27))
+
+
+def test_set_age_group():
+    assert "U13" == set_age_group(12, ["U13", "U15", "15+"])
+    assert "U15" == set_age_group(14,["U13","U15", "15+"])
+    assert "15+" == set_age_group(15, ["U13", "U15", "15+"])
+    assert "15+" == set_age_group(16, ["U13", "U15", "15+"])
+
 
 
 
