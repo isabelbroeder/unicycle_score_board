74ffbb52308f35de23cbc7678a1b317e2460bc0e
isabelbroeder <isabel.broeder@gmx.de>
2026-02-13 13:11:55 +0100

    calculate_results() is in calculate_results.py

--- src/unicycle/functions.py
+++ src/unicycle/functions.py
@@ -19,25 +19,8 @@ def calculate_age(date_of_birth: datetime, date: datetime) -> int:
     return age
 
 
-def calculate_results(category: str, age_group: str) -> pd.DataFrame:
-    # df_routines = DataLoader("../data/routines.db", "routines").get_data()
-    df_points = DataLoader("../../data/points.db", "points").get_data(
-        "SELECT *' FROM points WHERE age_group = ? AND category = ?",
-        [age_group, category],
-    )
-
-    # connection_points = sqlite3.connect("../data/points.db")
-    # cursor_points = connection_points.cursor()
-    # df_points = cursor_points.execute(
-    #    "SELECT * FROM points WHERE age_group = ? AND category = ?",
-    #    (age_group, category)
-    # ).fetchall()
-    print(df_points)
-
-
 def main():
-    # print(calculate_age(datetime.datetime(2002, 12, 4), datetime.datetime(2025, 3, 23)))
-    calculate_results("individual", "U21")
+    print(calculate_age(datetime.datetime(2002, 12, 4), datetime.datetime(2025, 3, 23)))
 
 
 if __name__ == "__main__":
