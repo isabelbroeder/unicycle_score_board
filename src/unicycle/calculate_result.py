a4bd7ac96ef9cc6e63dde9ed7591aa7850fa4701
isabelbroeder <isabel.broeder@gmx.de>
2026-03-09 15:11:19 +0100

    function calculate result

--- src/unicycle/calculate_result.py
+++ src/unicycle/calculate_result.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import itertools
+
+import numpy as np
 import pandas as pd
 
 from load_data import DataLoader
@@ -14,53 +16,50 @@ def calculate_result(category: str, age_group: str, judges: dict):
     d_judges = [judge for judge in judges if judge.startswith("D")]
 
     category = "'" + category + "'"  # must be in single quotes for sql
-    age_goup = "'" + age_group + "'"
+    age_group = "'" + age_group + "'"
 
-    # read database points
     df_points = DataLoader(
         Path(unicycle_score_board_path, "data/points.db"), "points"
     ).get_data(
-        f" SELECT * FROM points WHERE category == {category}  AND age_group == {age_goup}"
+        f" SELECT * FROM points WHERE category == {category}  AND age_group == {age_group}"
     )
-    print(df_points.columns)
 
     df_points.set_index("id_routine", inplace=True, drop=True)
-    df_points = df_points.apply(pd.to_numeric, errors="coerce")
-    print(df_points.columns)
-
-    # sum points per routine and judge
     df_sum_points_per_routine_per_judge = pd.DataFrame(index=df_points.index)
-    print(df_points.columns)
 
     for judge in itertools.chain(t_judges, p_judges):
-        relevant_cols = [f"{judge}_T", f"{judge}_S", f"{judge}_E"]
-        df_sum_points_per_routine_per_judge[judge] = df_points[relevant_cols].sum(
-            axis=1
-        )
+        columns = (df_points.columns[
+            df_points.columns.str.contains(judge)])
+        df_sum_points_per_routine_per_judge[judge] = df_points[
+            columns].sum(axis=1)
 
-    print(df_sum_points_per_routine_per_judge)
-    print(df_points.columns)
-    # percentage of points per routine and judge
+    divisor = 1
+    for d_judge in d_judges:
+        if category == "Small Group" or category == "Large Group":
+            divisor = np.sqrt(df_points[f"{d_judge}_N"])
+        df_sum_points_per_routine_per_judge[f"{d_judge}"] = 10 - (
+                    df_points[f"{d_judge}_S"] * 0.5 + df_points[f"{d_judge}_B"] / divisor)
+
     percentage_per_routine_per_judge = df_sum_points_per_routine_per_judge.div(
-        df_sum_points_per_routine_per_judge.sum(axis=0)
-    )
+        df_sum_points_per_routine_per_judge.sum(axis=0)) * 100
     print(percentage_per_routine_per_judge)
-    result = pd.DataFrame(index=df_points.index, columns=["T", "P", "D", "total"])
+    result = pd.DataFrame(index=df_points.index,
+                          columns=["T", "P", "D", "total"])
 
-    for judge_domain in ["T", "P"]:
-        relevant_cols = [judge for judge in judges if judge.startswith(judge_domain)]
-        print(relevant_cols)
-        print(percentage_per_routine_per_judge[relevant_cols].mean(axis=1))
-        result[judge_domain] = percentage_per_routine_per_judge[relevant_cols].mean(
-            axis=1
-        )
-    print(result)
+    for judge_domain in ["T", "P", "D"]:
+        relevant_cols = [
+            judge for judge in judges if judge.startswith(judge_domain)
+        ]
+        result[judge_domain] = percentage_per_routine_per_judge[
+            relevant_cols].mean(axis=1)
 
-    # calculate dismounts
-    print(df_points.columns)
+    f = lambda x: (x['T'] * 0.45 + x['P'] * 0.45 + x['D'] * 0.1)
+    result['total'] = result.apply(f, axis=1)
+    result["rank"] = result[["total", "T"]].apply(tuple, axis=1).rank(method='min', ascending=False).astype(int)
+    return result
 
 
-def generate_judges(t_count, p_count, d_count) -> list[str]:
+def generate_judges(t_p_count, d_count) -> list[str]:
     """
     generates list of judges
     t_count : number of technic judges
@@ -69,9 +68,8 @@ def generate_judges(t_count, p_count, d_count) -> list[str]:
     return: list of abbreviation of the judges like ['T1', 'T2' 'P1', 'P2', 'D1']
     """
     judges_list = []
-    for i in range(1, t_count + 1):
+    for i in range(1, t_p_count + 1):
         judges_list.append(f"T{i}")
-    for i in range(1, p_count + 1):
         judges_list.append(f"P{i}")
     for i in range(1, d_count + 1):
         judges_list.append(f"D{i}")
@@ -80,24 +78,21 @@ def generate_judges(t_count, p_count, d_count) -> list[str]:
 
 def create_judges():
     # number of judges in each category
-    individual_t = 4
-    individual_p = 4
-    individual_d = 2
-    pair_t = 4
-    pair_p = 4
-    pair_d = 2
-    small_group_t = 4
-    small_group_p = 4
-    small_group_d = 4
-    large_group_t = 4
-    large_group_p = 4
-    large_group_d = 4
+    number_t_p_judges = 4
+    number_d_judges_individual_pair = 2
+    number_d_judges_group = 4
     # Create dictionary
     judges = {
-        "individual": generate_judges(individual_t, individual_p, individual_d),
-        "pair": generate_judges(pair_t, pair_p, pair_d),
-        "small_group": generate_judges(small_group_t, small_group_p, small_group_d),
-        "large_group": generate_judges(large_group_t, large_group_p, large_group_d),
+        "individual female":
+            generate_judges(number_t_p_judges, number_d_judges_individual_pair),
+        "individual male":
+            generate_judges(number_t_p_judges, number_d_judges_individual_pair),
+        "pair":
+            generate_judges(number_t_p_judges, number_d_judges_individual_pair),
+        "small_group":
+            generate_judges(number_t_p_judges, number_d_judges_group),
+        "large_group":
+            generate_judges(number_t_p_judges, number_d_judges_group),
     }
     return judges
 
@@ -108,4 +103,4 @@ def create_judges():
 # judges = create_judges()
 # print(judges['single'])
 judges = create_judges()
-calculate_result("individual", "U21", judges)
+calculate_result("individual female", "U11", judges)
