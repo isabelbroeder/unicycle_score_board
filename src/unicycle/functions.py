fddae976c6510625e6a2fef3d63cb05a67a454f5
isabelbroeder <isabel.broeder@gmx.de>
2026-03-10 14:00:32 +0100

    ruff

--- src/unicycle/functions.py
+++ src/unicycle/functions.py
@@ -1,27 +1,20 @@
-"""calculates ages for different routine groups"""
-
 import datetime
 
-import pandas as pd
 
-from src.unicycle.load_data import DataLoader
-
-
-def calculate_age(date_of_birth: datetime, date: datetime) -> int:
+def calculate_age(date_of_birth: datetime, date: datetime = None) -> int:
     """
-    calculate the age at a given date
+    Calculate the age at a given date or today if not specified.
+
+    :param date_of_birth: The date of birth as a datetime object.
+    :param date: The date on which to calculate the age. Defaults to today if not provided.
+    :return: The age in years as an integer.
     """
+
+    if date is None:
+        date = datetime.today()
     age = date.year - date_of_birth.year
     if date_of_birth.month > date.month or (
-        date_of_birth.month == date.month and date_of_birth.day > date.day
-    ):  # had not yet had their birthday that year
+            date_of_birth.month == date.month and date_of_birth.day
+            > date.day):  # had not yet had their birthday that year
         return age - 1
     return age
-
-
-def main():
-    print(calculate_age(datetime.datetime(2002, 12, 4), datetime.datetime(2025, 3, 23)))
-
-
-if __name__ == "__main__":
-    main()
