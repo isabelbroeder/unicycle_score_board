2b5f69d1f6651476a656afacf4e6c5f6db4b9936
isabelbroeder <isabel.broeder@gmx.de>
2026-03-17 15:24:00 +0100

    Connect to database when database is initialized

--- src/unicycle/db_handler.py
+++ src/unicycle/db_handler.py
@@ -7,21 +7,33 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 
 
-
-
-
 class DbHandler(ABC):
     """Handles reading and writing data from SQLite databases."""
 
     def __init__(self, db_path: Path, table_name: str):
         self.db_path = db_path
         self.table_name = table_name
+        try:
+            self.db_connection = sqlite3.connect(self.db_path)
+            self.cursor = self.db_connection.cursor()
+        except Exception as e:
+            print(f"❌ Failed to connect database {self.table_name}: {e}")
+            self.is_connected = False
+        self.is_connected = True
+        sqlite3.register_adapter(datetime.date, adapt_date_iso)
+        sqlite3.register_converter("DATE", convert_date)
 
     @abstractmethod
     def create_table(self):
         pass
 
-    def get_data(self, sql_query: str = None, par=None) -> pd.DataFrame:  # contains code from `DataLoader.get_data()`#get_data(self, sql_query: str = None, par=None) -> pd.DataFrame:
+    def disconnect(self):
+        self.db_connection.close()
+        self.is_connected = False
+
+    def get_data(
+        self, sql_query: str = None, par=None
+    ) -> pd.DataFrame:  # contains code from `DataLoader.get_data()`#get_data(self, sql_query: str = None, par=None) -> pd.DataFrame:
         """
         Load data from a SQLite database into a pandas DataFrame.
 
@@ -45,82 +57,67 @@ class DbHandler(ABC):
 
         if sql_query is None:
             sql_query = f"SELECT * FROM {self.table_name}"
-        db_connection = None
         try:
-            db_connection = sqlite3.connect(self.db_path)
-            df = pd.read_sql_query(sql_query, con=db_connection, params=par)
-            db_connection.close()
+            df = pd.read_sql_query(sql_query, con=self.db_connection, params=par)
             return df
         except Exception as e:
             print(f"❌ Failed to load data from {self.table_name}: {e}")
             return pd.DataFrame()
-        finally:
-            if db_connection is not None:
-                db_connection.close()
 
-    def execute(self, sql_query: str, params = None):
-        db_connection = None
+
+    def execute(self, sql_query: str, params=None):
         try:
-            db_connection = sqlite3.connect(self.db_path)
-            cursor = db_connection.cursor()
             if params is None:
-                cursor.execute(sql_query)
+                self.cursor.execute(sql_query)
             else:
-                cursor.executemany(sql_query,params)
-            db_connection.commit()
+                for param in params:
+                    self.cursor.execute(sql_query, param)
+            self.db_connection.commit()
         except Exception as e:
             print(f"❌ Error executing sql query on {self.table_name}: {e}")
-        finally:
-            if db_connection is not None:
-                db_connection.close()
+
 
     def update_data(self, df: pd.DataFrame):
-        db_connection = None
         try:
-            db_connection = sqlite3.connect(self.db_path)
-            df.to_sql(self.table_name, db_connection, if_exists="replace", index=False)
-            db_connection.close()
+            df.to_sql(
+                self.table_name, self.db_connection, if_exists="replace", index=False
+            )
             print(f"✅ {self.table_name} updated successfully.")
         except Exception as e:
             print(f"❌ Error writing to {self.table_name}: {e}")
-        finally:
-            if db_connection is not None:
-                db_connection.close()
 
 
-    def update_multiple_rows(self, df: pd.DataFrame, key_columns: list,
-                             update_columns: list):
+    def update_multiple_rows(
+        self, df: pd.DataFrame, key_columns: list, update_columns: list
+    ):
         try:
-            conn = sqlite3.connect(self.db_path)
-            cursor = conn.cursor()
-
             sql = f"""
             UPDATE {self.table_name}
             SET {', '.join([f"{col} = ?" for col in update_columns])}
             WHERE {" AND ".join([f"{col} = ?" for col in key_columns])}
             """
-
-            data = [[row[col] for col in update_columns] +
-                    [row[col] for col in key_columns]
-                    for _, row in df.iterrows()]
-
-            cursor.executemany(sql, data)
-            conn.commit()
-            conn.close()
-            print(
-                f"✅ {update_columns} in {self.table_name} updated successfully."
-            )
+            data = [
+                [row[col] for col in update_columns] + [row[col] for col in key_columns]
+                for _, row in df.iterrows()
+            ]
+            self.cursor.executemany(sql, data)
+            self.db_connection.commit()
+            print(f"✅ {update_columns} in {self.table_name} updated successfully.")
         except Exception as e:
-            print(
-                f"❌ Error updating {update_columns} in {self.table_name}: {e}")
+            print(f"❌ Error updating {update_columns} in {self.table_name}: {e}")
 
+
+    def last_row_id(self):
+        return self.cursor.lastrowid
 
 
 def adapt_date_iso(val):
     """Adapt datetime.date to ISO 8601 date."""
+    print("adapt date return:", val.isoformat())
     return val.isoformat()
 
 
 def convert_date(val):
     """Convert ISO 8601 date to datetime.date object."""
+    print("convert date return: ", datetime.date.fromisoformat(val.decode()))
     return datetime.date.fromisoformat(val.decode())
