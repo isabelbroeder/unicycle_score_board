246491e0393bcd39b3ae070fff569243de7dfdc9
isabelbroeder <isabel.broeder@gmx.de>
2026-03-27 11:13:32 +0100

    test singleton pattern

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import datetime
+
 from src.unicycle.functions import calculate_age
 from src.unicycle.create_database import set_age_group
 from src.unicycle.riders_db_handler import RidersDbHandler
@@ -54,3 +55,19 @@ def test_database():
     assert (
         (df["name"] == "Max Mustermann") & (df["routine_name"] == "Max und Moritz")
     ).any()
+
+def test_singleton_pattern_riders():
+    riders_db_handler1 = RidersDbHandler()
+    riders_db_handler2 = RidersDbHandler()
+    assert riders_db_handler1 == riders_db_handler2
+
+def test_singleton_pattern_riders_routines():
+    riders_routines_db_handler1 = RidersRoutinesDbHandler()
+    riders_routines_db_handler2 = RidersRoutinesDbHandler()
+    assert riders_routines_db_handler1 == riders_routines_db_handler2
+
+def test_singleton_pattern_routines():
+    routines_db_handler1 = RoutinesDbHandler()
+    routines_db_handler2 = RoutinesDbHandler()
+    assert routines_db_handler1 == routines_db_handler2
+
