ea7d770c94b2173e7291f46acb7070b821a93ed7
isabelbroeder <isabel.broeder@gmx.de>
2026-03-31 13:33:03 +0200

    tests in several tests files

---
+++ test/test_singleton_pattern.py
@@ -0,0 +1,19 @@
+from src.unicycle.db_handler.riders_db_handler import RidersDbHandler
+from src.unicycle.db_handler.riders_routines_db_handler import RidersRoutinesDbHandler
+from src.unicycle.db_handler.routines_db_handler import RoutinesDbHandler
+
+
+def test_singleton_pattern_riders():
+    riders_db_handler1 = RidersDbHandler()
+    riders_db_handler2 = RidersDbHandler()
+    assert riders_db_handler1 == riders_db_handler2
+
+def test_singleton_pattern_riders_routines():
+    riders_routines_db_handler1 = RidersRoutinesDbHandler()
+    riders_routines_db_handler2 = RidersRoutinesDbHandler()
+    assert riders_routines_db_handler1 == riders_routines_db_handler2
+
+def test_singleton_pattern_routines():
+    routines_db_handler1 = RoutinesDbHandler()
+    routines_db_handler2 = RoutinesDbHandler()
+    assert routines_db_handler1 == routines_db_handler2
